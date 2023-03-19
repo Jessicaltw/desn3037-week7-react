15429bda73d1bc9d7c95b933bf348499acf55278
Jessicaltw <Jessicaltwing@gmail.com>
2023-03-19 03:56:08 -0400

    Populating individual pages and calibrating type

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Box, Container } from '@mui/system';
+
 import * as React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
@@ -8,9 +14,13 @@ import Menu from "./components/Menu";
 function App() {
   return (
     <div className="App">
-
+      <Container maxWidth="sm" sx={{
+        mt:5
+      }}>
 <BrowserRouter>
+<Box sx={{mb:2}}>
      <Menu />
+     </Box>
     <Routes>
   
      <Route exact path="about" element={<About />} />
@@ -21,6 +31,7 @@ function App() {
   
 
 </BrowserRouter>
+</Container>
     </div>
   );
 }
